82943fe57a24dbfbdffffe3a494f4d2023f3e493
mostafa <mstfymhmdsamy@gmail.com>
2022-02-20 11:18:41 +0200

    GulpBasics

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// const gulp = require('gulp')
+
+
+// const gulpsass = require('gulp-sass')
+// const sass = gulpsass(require('sass'))
+// gulp.task('sas', async function() {
+//     gulp.src('style.scss').pipe(sass()).pipe(gulp.dest('css'))
+// })
